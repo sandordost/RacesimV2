b54e09ed749b7379dfc421b398a165162380cac5
sandordost <sandor1998@hotmail.com>
2024-01-23 20:23:02 +0100

    Update TeamColorToBrushConverter.cs

--- RaceSimulatorSolution/RaceSimulatorWPF/TeamColorToBrushConverter.cs
+++ RaceSimulatorSolution/RaceSimulatorWPF/TeamColorToBrushConverter.cs
@@ -1,37 +1,36 @@
-﻿using RaceSimulatorShared.Models.Competitions.Participants;
+﻿using RaceSimulatorShared.Models.Participants;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace RaceSimulatorWPF
+namespace RaceSimulatorWPF;
+
+public class TeamColorToBrushConverter : IValueConverter
 {
-    public class TeamColorToBrushConverter : IValueConverter
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        if (value is TeamColor teamColor)
         {
-            if (value is TeamColor teamColor)
+            Color color = teamColor switch
             {
-                Color color = teamColor switch
-                {
-                    TeamColor.Red => Colors.Red,
-                    TeamColor.Green => Colors.Green,
-                    TeamColor.Blue => Colors.Blue,
-                    TeamColor.Yellow => Colors.Yellow,
-                    TeamColor.Grey => Colors.Gray,
-                    _ => Colors.Transparent,
-                };
+                TeamColor.Red => Colors.Red,
+                TeamColor.Green => Colors.Green,
+                TeamColor.Blue => Colors.Blue,
+                TeamColor.Yellow => Colors.Yellow,
+                TeamColor.Grey => Colors.Gray,
+                _ => Colors.Transparent,
+            };
 
-                Color darkerColor = Color.Multiply(color, 0.7f);
+            Color darkerColor = Color.Multiply(color, 0.7f);
 
-                return new SolidColorBrush(darkerColor);
-            }
-
-            return Brushes.Transparent;
+            return new SolidColorBrush(darkerColor);
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        return Brushes.Transparent;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
     }
 }
